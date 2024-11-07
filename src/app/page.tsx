0771377a911f2c815b73f6f724711780bf339036
umamarajpoot <umamarajpoottasswor@gmail.com>
2024-11-07 13:02:06 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Calculator from './components/Calculator';
+
+const Home: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <Calculator />
+    </div>
+  );
+};
+
+export default Home;
